91eca212c2f05afcd47b3c2b7cd143fa4517fc2c
jbrigido <jbrigido_14@hotmail.com>
2023-03-30 23:14:49 +0100

    Exercicio data e hora

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+//Exercicio tipos de variáveis
+
 var typeString = "Uma variável do tipo String";
 var typeNumber = 14;
 var typeBoolean = true;
 
 console.log(typeString, " ", typeNumber, " ", typeBoolean);
+
+/*------------------------------------------------------------------------------------------
+Exercicio mostrar data e hora num elemento HTML*/
+
+const fullDate = new Date ();
+const time = new Date ().toString().split(" ")[4];
+var date = '';
+
+var day = fullDate.getDate();
+var month = fullDate.getMonth();
+var year = fullDate.getFullYear();
+
+date = day + "-" + (month+1) + "-" + year;
+
+document.getElementById('data').innerHTML = date;
+document.getElementById('hora').innerHTML = time;
+
